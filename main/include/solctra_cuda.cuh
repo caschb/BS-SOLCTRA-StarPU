7af8d5d62709e852ad4059f8fd77f23fd502daf9
Christian Asch <casch@cenat.ac.cr>
2024-03-07 08:30:53 -0600

    Renamed file to reflect that it is a CUDA header

---
+++ main/include/solctra_cuda.cuh
@@ -0,0 +1,9 @@
+#ifndef SOLCTRA_CUDA_H
+#define SOLCTRA_CUDA_H
+
+#include <utils.h>
+
+__global__ void runParticles_gpu(Coils &coils, Coils &e_roof, LengthSegments &length_segments,
+                  Particle * &particles,
+                  const unsigned int steps, const double step_size, const unsigned int mode, const unsigned int id);
+#endif
